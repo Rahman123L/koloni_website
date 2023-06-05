235e150f32fb6af85f5482a03245fe582eee1607
My-profilee <cantikdj24@gmail.com>
2023-06-05 14:54:41 +0700

    update

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,9 +3,19 @@
 namespace FebriRahman\Sambak\Controller;
 
 use FebriRahman\Sambak\App\View;
+use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
 
 class ProductController
 {
+    static Client $client;
+    static ResponseInterface $data;
+
+    public function __construct()
+    {
+        self::$client = new Client();
+    }
+
     function checkout()
     {
         View::render('Products/checkout', [
@@ -29,8 +39,14 @@ class ProductController
 
     function productDetail()
     {
+        $id = $_GET['id'];
+        self::$data = self::$client->request('GET', "https://fakestoreapi.com/products/$id");
+        $response = self::$data->getBody()->getContents();
+        $data = json_decode($response, true);
+
         View::render('products/productDetail', [
-            "title" => "Product Name"
+            "title" => "Product Name",
+            "product" => $data
         ]);
     }
 
--- src/View/Products/productDetail.php
+++ src/View/Products/productDetail.php
@@ -2,25 +2,16 @@
     <div class="container px-6 py-10 mx-auto">
         <div class="lg:-mx-6 lg:flex lg:items-center">
         
-            <img class="lg:w-1/2 lg:mx-6 w-full h-96 rounded-lg lg:h-[25rem] dark:bg-gray-700" src="../images/category-1.png" alt="">
+            <img class="lg:w-1/2 lg:mx-6 w-full h-96 rounded-lg lg:h-[25rem] dark:bg-gray-700" src="<?= $model['product']['image']; ?>" alt="">
 
             <div class="mt-8 lg:w-1/2 lg:px-6 lg:mt-0">
 
                 <h1 class="text-2xl font-semibold text-gray-800 dark:text-white lg:text-3xl lg:w-96">
-                    Kerja Bagus
+                    <?= $model['product']['title']; ?>
                 </h1>
 
                 <p class="max-w-lg mt-6 text-gray-500 dark:text-gray-400 ">
-                    “ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero ad
-                    tempora doloribus expedita laborum saepe voluptas perferendis delectus assumenda rerum, culpa
-                    aperiam dolorum, obcaecati corrupti aspernatur a. ”
-                </p>
-
-                <h3 class="mt-6 text-lg font-medium text-blue-500">Mia Brown</h3>
-                <p class="text-gray-600 dark:text-gray-300">
-                    “ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero ad
-                    tempora doloribus expedita laborum saepe voluptas perferendis delectus assumenda rerum, culpa
-                    aperiam dolorum, obcaecati corrupti aspernatur a. ”
+                <?= $model['product']['description']; ?>
                 </p>
                 
                 <div class="flex">
